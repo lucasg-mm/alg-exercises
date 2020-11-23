e377f77d7d7f190ad0639cf61d86d32bf4f5e826
Lucas Miranda <lucasalternativo.lg@gmail.com>
2020-11-23 14:27:43 -0300

    Termina ex 12 (mais ou menos)

--- e12/main.py
+++ e12/main.py
@@ -1,39 +1,58 @@
-def get_lis(list_of_dims):
+def get_lis(arr):
     """
-    Computes the longest increasing subsequence (LIS) of 'list_of_dims'. Returns
-    a list with every element of the LIS. We use dynamic programming to do this.
-    Note that 'list_of_dims' is a list of tuples, w/ each tuple being of the (w, h)
-    format, such that 'w' stands for width and 'h', for height.   
+    Find the longest increasing subsequence using a dynamic programming approach.
     """
-    n = len(list_of_dims)
-    list_of_dims = sorted(list_of_dims, key = lambda dim: dim[0])  # sorting the list of tuples by width
+    n = len(arr)
+    arr = sorted(arr, key = lambda dim: dim[0])  # sorting the list of tuples by width
 
     # buids the memoization matrix (initially, it's all set up to be zero):
-    memoization = [[0 for j in range(n + 1)]
-                   for i in range(n + 1)]
+    M = [[0 for j in range(n)]
+                   for i in range(n)]
 
-    # we use the dynamic programming iteration to evaluate every entry of 'memoization'
-    for index_curr in range(1, n + 1):
-        for index_next in range(n + 1):
-            if index_next != n and (list_of_dims[index_curr - 1][1] > list_of_dims[index_next][1] or index_curr - 1 >= index_next):
-                memoization[index_curr][index_next] = memoization[index_curr - 1][index_next]
+    # we use the dynamic programming iteration to evaluate every entry of 'M'
+    for i in range(n):
+        if i == 0:
+            for j in range(i, n):
+                if j < n - 1 and (arr[i][1] >= arr[j+1][1] or arr[i][0] == arr[j+1][0]):
+                    M[i][j] = 0
+                else:
+                    M[i][j] = 1    
+        else:
+            for j in range(i, n):
+                if j < n - 1 and (arr[i][1] >= arr[j+1][1] or arr[i][0] == arr[j+1][0]):
+                    M[i][j] = M[i-1][j]
+                else:
+                    M[i][j] = max(M[i-1][j], 1 + M[i-1][i-1])
+
+    return trace_back_iterative(M, arr) # returns a set with every element of the lis                            
+            
+def trace_back_iterative(M, arr):
+    lis = set()
+    n = len(arr)
+    i = n - 1
+    j = n - 1
+
+    while i >= 0 and j >= 0:
+        if j >= i:
+            if i == 0:
+                if j < n - 1 and (arr[i][1] >= arr[j+1][1] or arr[i][0] == arr[j+1][0]):
+                    i -= 1
+                else:
+                    lis.add(arr[i])
+                    i -= 1
+                    j = i           
             else:
-                memoization[index_curr][index_next] = max(
-                    memoization[index_curr - 1][index_next], 1 + memoization[index_curr - 1][index_curr - 1])                       
+                if j < n - 1 and (arr[i][1] >= arr[j+1][1] or arr[i][0] == arr[j+1][0]):
+                    i -= 1
+                else:
+                    if 1 + M[i-1][i-1] >= M[i-1][j]:
+                        lis.add(arr[i])
+                        i -= 1
+                        j = i
+                    else:
+                        i -= 1
 
-    return trace_back(memoization, list_of_dims, n, n, n, set())
-
-def trace_back(memoization, list_of_dims, index_curr, index_next, n, lis):
-    if index_curr != 0 and index_next != 0:
-        if index_next != n and (list_of_dims[index_curr - 1][1] > list_of_dims[index_next][1] or index_curr - 1 >= index_next):
-            trace_back(memoization, list_of_dims, index_curr - 1, index_next, n, lis)
-        else:    
-            if 1 + memoization[index_curr - 1][index_curr - 1] >= memoization[index_curr - 1][index_next]:
-                lis.add(list_of_dims[index_curr - 1])
-                trace_back(memoization, list_of_dims, index_curr - 1, index_curr - 1, n, lis)
-            else:                           
-                trace_back(memoization, list_of_dims, index_curr - 1, index_next, n, lis)
-    return lis            
+    return lis
 
 def main():
     # get number of test cases
@@ -41,9 +60,13 @@ def main():
 
     for _ in range(num_test_cases):
         # user input for one test case
-        input()  # get the number of dolls 
+        num_dolls = int(input())  # get the number of dolls 
         dims = input()  # get the dimensions as string in one line
         dims = dims.split()  # split it at space
+        while len(dims) / 2 != num_dolls:
+            aux_dims = input()  # when the dimensions are given in different lines
+            aux_dims = aux_dims.split()
+            dims.extend(aux_dims)
         dims = [int(dim) for dim in dims]  # parse everything to int
         dims = [(dims[i], dims[i+1]) for i in range(0, len(dims) - 1, 2)]  # put every couple of dimensions (width and height) in a tuple 
         
